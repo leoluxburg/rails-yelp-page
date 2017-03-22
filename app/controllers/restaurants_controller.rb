bc1a8f11efa1c9b48df73d1feb2d0eebfeb00324
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-03-22 16:51:55 +0100

    degusta

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.new(restaurants_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def restaurants_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Slabon", address: "Obarrio", phone_number: "+507 317-0062", category: "belgian")
+Restaurant.create(name: "Dainer 16", address: "San Francisco", phone_number: "+507 214- 9470", category: "belgian")
+Restaurant.create(name: "Ciao Ragatzi", address: "Clayton", phone_number: "+507 317-0976", category: "italian")
+Restaurant.create(name: "Lung fung", address: "El Dorado", phone_number: "+507 317-0976", category: "chinese")
+Restaurant.create(name: "Sushi House", address: "Albrook", phone_number: "+507 214-9490", category: "japanese")
+
+
